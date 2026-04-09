293295f3f855724c4a1a55906bb5f2d83dad07d6
barusev-gif <severovabarbora@gmail.com>
2026-04-09 13:39:15 +0200

    Update basic.sql

--- sql/basic.sql
+++ sql/basic.sql
@@ -1,3 +1,28 @@
 -- SQL basics
 
 SELECT * FROM tickets;
+-- BASIC SELECT
+
+-- Vybere všechna data z tabulky
+SELECT * FROM tickets;
+
+-- Vybere jen jeden sloupec (title)
+SELECT title FROM tickets;
+
+-- Vybere více sloupců
+SELECT title, assignee FROM tickets;
+
+-- Filtr podle statusu
+SELECT * FROM tickets WHERE status = 'Open';
+
+-- Filtr podle assignee
+SELECT title FROM tickets WHERE assignee = 'Anna';
+
+-- POZNÁMKY:
+
+-- SELECT = co chci vidět
+-- FROM = z jaké tabulky
+-- WHERE = podmínka (filtr)
+
+-- * znamená všechny sloupce
+-- ; ukončuje daný dotaz 
